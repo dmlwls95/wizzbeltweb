310ee2c1c565476b3b35a06ad141091afc92c7b4
raco0n <skrgns0411@naver.com>
2018-06-21 03:26:28 +0900

    0621 등록 주석

--- public/js/item_reg.js
+++ public/js/item_reg.js
@@ -8,38 +8,38 @@ $(function()
     //승인요청버튼 눌렀을 때 작동하는 함수 function click
     $("#Request").click(function()
     { 
-        var brand_name  = $('#brand_name').val(); 
-        var item_name = $('#item_name').val(); 
-        var main_contents = $('#main_contents').val(); 
-        var notice_order = $('#notice_order').val(); 
-        var info_file1 = $('#info_file1').val(); 
-        var info_file2 = $('#info_file2').val(); 
-        var info_file3 = $('#info_file3').val(); 
-        var supply_price = $('#supply_price').val(); 
-        var customer_price = $('#customer_price').val(); 
-        var item_origin = $('#item_origin').val(); 
-        var weight = $('#weight').val(); 
-        var youtube_url = $('#youtube_url').val(); 
-        var agreement = 0;
+        var brand_name  = $('#brand_name').val(); //브랜드이름
+        var item_name = $('#item_name').val();  //상품명
+        var main_contents = $('#main_contents').val(); //상품설명
+        var notice_order = $('#notice_order').val(); //주문시 주의사항
+        var info_file1 = $('#info_file1').val(); //첨부파일 1~3에 대한 설명
+        var info_file2 = $('#info_file2').val(); //""
+        var info_file3 = $('#info_file3').val(); //""
+        var supply_price = $('#supply_price').val(); //공급가
+        var customer_price = $('#customer_price').val(); //소비자가
+        var item_origin = $('#item_origin').val(); //원산지
+        var weight = $('#weight').val(); //상품무게 kg단위
+        var youtube_url = $('#youtube_url').val(); //유투브 동영상 url
+        var agreement = 0; //승인여부 승인 1, 미승인 0
 
             firebaseEmailAuth.onAuthStateChanged(function (user) 
             {
-                var userInfo = user;
+                var userInfo = user; //사용자정보 객체 생성및 삽입
 
                 if(brand_name.length == 0 || item_name.length == 0 || supply_price.length == 0 || item_origin.length == 0 || customer_price.length == 0 || weight.length == 0)
-                {
+                {//제약조건: 브랜드이름, 상품명, 공급가, 소비자가, 원산지, 상품무게 (필수기입)
                     alert("필수 입력정보에 빈칸이 없는지 다시 확인해 주세요."); 
-                    return;  
+                    return;  //제약조건에 걸리면 alert띄우고 리턴
                 }
                 else{       
-                dorequest(userInfo);                
+                dorequest(userInfo); //조건 통과시 dorequest로 DB 삽입
                 }                
             }); 
 
             function dorequest(userInfo)
             { 
                 firebaseDatabase.ref("SCM_Item_Reg_Request_List/"+ brand_name).set(
-                    {
+                    {   //최상위 디렉터리 SCM_.../브랜드이름을 하위키로 삽입 //기본키 유일식별성 생각하기!
                         D00_agreement: agreement,
                         D01_brand_name: brand_name,
                         D02_item_name: item_name,
@@ -56,8 +56,8 @@ $(function()
                     }
                 )
                 .then(function(){
-                alert(brand_name+"님 승인요청이 완료되었습니다.\n게시가 완료되면 알려드리겠습니다. :)");
-                window.location.replace('../main.html');
+                alert(brand_name+"님 승인요청이 완료되었습니다.\n게시가 완료되면 알려드리겠습니다. \n:)");
+                window.location.replace('../main.html');// 승인요청이후 main으로 페이지 이동
                 });
             };             
     }); 
